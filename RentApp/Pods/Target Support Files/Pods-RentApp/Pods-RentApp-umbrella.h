b25569f3b563e17893f58b33975f2f8a4e10a063
Adarsh Joshi <adarshjoshi@Adarshs-MacBook-Pro-2.local>
2016-05-14 13:32:21 -0700

    Facebook and Google Authentication fixed, Home Page view fixed

--- RentApp/Pods/Target Support Files/Pods-RentApp/Pods-RentApp-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_RentAppVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RentAppVersionString[];
-
--- RentApp/Pods/Target Support Files/Pods-RentAppUITests/Pods-RentAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RentAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RentAppUITests
-@end
